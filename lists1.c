068bc046b540f0cb16a860414a0d4b8780e64a3a
HaydarKhalife <146635050+HaydarKhalife@users.noreply.github.com>
2023-12-18 15:39:37 +0200

    haha

--- lists1.c
+++ lists1.c
@@ -105,7 +105,7 @@ list_t *node_starts_with(list_t *node, char *prefix, char c)
  * get_node_index - gets the index of a node
  * @head: pointer to list head
  * @node: pointer to the node
- *
+ *  haha
  * Return: index of node or -1
  */
 ssize_t get_node_index(list_t *head, list_t *node)
